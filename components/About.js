045356137ebf171016f92e6d85f61b25baa2a16b
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-07 08:43:40 -0400

    feat: updating about section

--- components/About.js
+++ components/About.js
@@ -9,19 +9,13 @@ function About() {
 						Kellen Wiltshire
 					</h1>
 					<p className='text-sm sm:text-xl leading-relaxed font-body pb-5'>
-						During this COVID-19 Pandemic, I have taught myself two new things.
-						First, I can now make bread like nobody's business. Second, I
-						learned how to build sweet websites. Though I am still starting out,
-						I am eager to learn more and dive deeper into the world of Web
-						Development
-					</p>
-
-					<p className='text-sm sm:text-xl pb-5 leading-relaxed font-body'>
-						Although I have a fulfilling career helping people seek justice in
-						the Legal System, I was missing using my brain in a technical role.
-						With the extra time working from home I decided to start teaching
-						myself Web Development with the help of the Udemy course
-						Zero-to-Mastery.
+						I am a passionate full-stack developer specializing in all aspects
+						of Web Development looking for the next step in my career. I have
+						the experience to create beautiful and functional websites combining
+						the latest frameworks and technologies. I have experience in React,
+						Javascript/Typescript, TailwindCSS, Styled Components, as well as
+						traditional HTML5 and CSS3. I am currently look for a new Web
+						Development career opportunity!
 					</p>
 
 					<p className='text-sm sm:text-xl pb-5 leading-relaxed font-body'>
