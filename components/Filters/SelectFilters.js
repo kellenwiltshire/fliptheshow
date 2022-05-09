72432b452e41480334f717c8a30a92e4b6ca36ba
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-05-09 16:53:48 -0400

    feat: better layout on smaller tablet devices

--- components/Filters/SelectFilters.js
+++ components/Filters/SelectFilters.js
@@ -3,12 +3,8 @@ import React from 'react';
 function SelectFilters({ setValue, options, defaultValue }) {
 	return (
 		<select
-			className='rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10'
-			onChange={(e) =>
-				e.target.value === { defaultValue }
-					? setValue('')
-					: setValue(e.target.value)
-			}
+			className='rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base lg:pl-3 lg:pr-10 w-full'
+			onChange={(e) => (e.target.value === { defaultValue } ? setValue('') : setValue(e.target.value))}
 			placeholder={defaultValue}
 		>
 			{options.map((item) => {
