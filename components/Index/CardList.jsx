34d877244f875cb212d73625a07f19045fe8b55c
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-17 07:36:28 -0400

    refactor: refactor prep for styling

--- components/Index/CardList.jsx
+++ components/Index/CardList.jsx
@@ -18,23 +18,21 @@ const markets = [
 	},
 ];
 
-export default function Cards() {
+export default function CardsList() {
 	return (
-		<div className='bg-white'>
-			<div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 '>
-				<div className='space-y-12'>
-					<div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
-						<h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>View Markets</h2>
-					</div>
-					<ul
-						role='list'
-						className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'
-					>
-						{markets.map((market) => (
-							<CardLink market={market} />
-						))}
-					</ul>
+		<div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 '>
+			<div className='space-y-12'>
+				<div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
+					<h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>View Markets</h2>
 				</div>
+				<ul
+					role='list'
+					className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'
+				>
+					{markets.map((market) => (
+						<CardLink market={market} />
+					))}
+				</ul>
 			</div>
 		</div>
 	);
