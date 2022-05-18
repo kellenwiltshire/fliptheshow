c4150528284a8186309d28f0cc47514b3e449c31
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-18 10:57:37 -0400

    feat: Layout style implemented

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import Head from 'next/head';
 import Navigation from './Navigation';
 import Footer from './Footer';
+import styled from '@emotion/styled';
+
+export const Container = styled.main`
+	margin-left: auto;
+	margin-right: auto;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+`;
 
 function Layout({ children }) {
 	return (
 		<div>
 			<Head>
 				<link rel='icon' href='/favicon.ico' />
-				<meta description='Flip The Show MLB21 The Show 21 TheShow21 Baseball Cards Flipping Market Xbox Playstation' />
+				<meta description='Flip The Show MLB The Show 22 TheShow 22 Baseball Cards Flipping Market Xbox Playstation' />
 				<meta name='Flip The Show' />
 				<meta lang='en' />
 			</Head>
 			<Navigation />
-			<main className='mx-auto min-h-screen flex justify-center flex-row flex-wrap'>
-				{children}
-			</main>
+			<Container>{children}</Container>
 			<Footer />
 		</div>
 	);
