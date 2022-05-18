cc48c08541e8401bcbc7db4f33cea2b422716281
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-18 11:04:43 -0400

    refactor footer style

--- components/Layout/Styles/Style-Footer.jsx
+++ components/Layout/Styles/Style-Footer.jsx
@@ -20,6 +20,15 @@ export const FooterStyle = styled.footer`
 	@media (min-width: 768px) {
 		max-width: 768px;
 	}
+	@media (min-width: 1024px) {
+		max-width: 1024px;
+	}
+	@media (min-width: 1280px) {
+		max-width: 1280px;
+	}
+	@media (min-width: 1536px) {
+		max-width: 1536px;
+	}
 `;
 
 export const TitleName = styled.p`
