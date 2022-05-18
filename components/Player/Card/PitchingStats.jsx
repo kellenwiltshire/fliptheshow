dc17905dcfd51dedbbecddb419bed81bef787b9b
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-18 14:51:13 -0400

    feat: styling for Ptiching Stats

--- components/Player/Card/PitchingStats.jsx
+++ components/Player/Card/PitchingStats.jsx
@@ -1,76 +1,66 @@
 import React from 'react';
+import { PitchingContainer, PitchingHeader, Stat, StatsHolder } from '../Styles/PitchingStats';
 
 function PitchingStats({ data }) {
 	return (
-		<div>
-			<div className='flex-row flex-wrap flex my-2 justify-center'>
-				<h1 className='w-full underline text-lg'>Pitching Stats</h1>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>Hits/BF</span>
-					<span className='w-20 text-center'>{data.hits_per_bf}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>HR/BF</span>
-					<span className='w-20 text-center'>{data.hr_per_bf}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>K/BF</span>
-					<span className='w-20 text-center'>{data.k_per_bf}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>Pitch Control</span>
-					<span className='w-20 text-center'>{data.pitch_control}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>Pitch Movement</span>
-					<span className='w-20 text-center'>{data.pitch_movement}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>Pitch Velocity</span>
-					<span className='w-20 text-center'>{data.pitch_velocity}</span>
-				</div>
-				<div className='flex-col flex border border-gray-200 px-2'>
-					<span className='w-20 text-center'>Clutch</span>
-					<span className='w-20 text-center'>{data.pitching_clutch}</span>
-				</div>
-				<div className='flex-col flex m-2'>
-					<span className='w-full text-lg text-center'>Pitches</span>
-					<div className='w-full flex flex-row flex-wrap'>
-						{data.pitches.length
-							? data.pitches.map((pitch) => {
-									return (
-										<div
-											key={Math.random()}
-											className='flex-row flex-wrap justify-center flex my-2'
-										>
-											<span className='w-full underline text-center'>
-												{pitch.name}
-											</span>
+		<PitchingContainer>
+			<PitchingHeader>Pitching Stats</PitchingHeader>
+			<StatsHolder>
+				<Stat>Hits/BF</Stat>
+				<Stat>{data.hits_per_bf}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>HR/BF</Stat>
+				<Stat>{data.hr_per_bf}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>K/BF</Stat>
+				<Stat>{data.k_per_bf}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>Pitch Control</Stat>
+				<Stat>{data.pitch_control}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>Pitch Movement</Stat>
+				<Stat>{data.pitch_movement}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>Pitch Velocity</Stat>
+				<Stat>{data.pitch_velocity}</Stat>
+			</StatsHolder>
+			<StatsHolder>
+				<Stat>Clutch</Stat>
+				<Stat>{data.pitching_clutch}</Stat>
+			</StatsHolder>
+			<div className='flex-col flex m-2'>
+				<span className='w-full text-lg text-center'>Pitches</span>
+				<div className='w-full flex flex-row flex-wrap'>
+					{data.pitches.length
+						? data.pitches.map((pitch) => {
+								return (
+									<div key={pitch.name} className='flex-row flex-wrap justify-center flex my-2'>
+										<span className='w-full underline text-center'>{pitch.name}</span>
 
-											<div className='flex-col flex border border-gray-200 px-2'>
-												<span className='w-20 text-center'>Control</span>
-												<span className='w-20 text-center'>
-													{pitch.control}
-												</span>
-											</div>
-											<div className='flex-col flex border border-gray-200 px-2'>
-												<span className='w-20 text-center'>Movement</span>
-												<span className='w-20 text-center'>
-													{pitch.movement}
-												</span>
-											</div>
-											<div className='flex-col flex border border-gray-200 px-2'>
-												<span className='w-20 text-center'>Speed</span>
-												<span className='w-20 text-center'>{pitch.speed}</span>
-											</div>
+										<div className='flex-col flex border border-gray-200 px-2'>
+											<Stat>Control</Stat>
+											<Stat>{pitch.control}</Stat>
 										</div>
-									);
-							  })
-							: null}
-					</div>
+										<div className='flex-col flex border border-gray-200 px-2'>
+											<Stat>Movement</Stat>
+											<Stat>{pitch.movement}</Stat>
+										</div>
+										<div className='flex-col flex border border-gray-200 px-2'>
+											<Stat>Speed</Stat>
+											<Stat>{pitch.speed}</Stat>
+										</div>
+									</div>
+								);
+						  })
+						: null}
 				</div>
 			</div>
-		</div>
+		</PitchingContainer>
 	);
 }
 
