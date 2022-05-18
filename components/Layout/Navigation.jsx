f6fe8a7b87d473c099f0a1082dd2a1986b81915d
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-18 11:24:06 -0400

    feat: Navigation Style Implemented

--- components/Layout/Navigation.jsx
+++ components/Layout/Navigation.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
-import { Container, NavContainer } from './Styles/Style-Navigation';
+import { Container, DonateLink, HomeLink, Nav, NavContainer, NavLink } from './Styles/Style-Navigation';
 
 function Navigation() {
 	return (
 		<Container>
 			<NavContainer>
 				<Link href='/'>
-					<a className='hover:text-gray-900 py-1 text-center w-full md:w-auto md:py-1 md:my-2 md:ml-2'>Flip The Show</a>
+					<HomeLink>Flip The Show</HomeLink>
 				</Link>
-				<nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center my-2 lg:my-0'>
+				<Nav>
 					<Link href='/playercards'>
-						<a className='mr-5 hover:text-gray-900'>Players</a>
+						<NavLink>Players</NavLink>
 					</Link>
 
 					<Link href='/stadiums'>
-						<a className='mr-5 hover:text-gray-900'>Stadiums</a>
+						<NavLink>Stadiums</NavLink>
 					</Link>
 					<Link href='/equipment'>
-						<a className='mr-5 hover:text-gray-900'>Equipment</a>
+						<NavLink>Equipment</NavLink>
 					</Link>
 					<Link href='/contact'>
-						<a className='mr-5 hover:text-gray-900'>Contact</a>
+						<NavLink>Contact</NavLink>
 					</Link>
-				</nav>
-				<div className='mt-3 lg:mt-0 hidden md:block'>
-					<a
-						href='https://www.paypal.com/donate?business=Z2PC95TA9JLVL&item_name=Web+Services&currency_code=CAD'
-						rel='noopener noreferrer'
-						target='_blank'
-					>
-						<img src='/donate.png' alt='Donate with PayPal' width='90px' height='auto' />
-					</a>
-				</div>
+				</Nav>
+				<DonateLink
+					href='https://www.paypal.com/donate?business=Z2PC95TA9JLVL&item_name=Web+Services&currency_code=CAD'
+					rel='noopener noreferrer'
+					target='_blank'
+				>
+					<img src='/donate.png' alt='Donate with PayPal' width='90px' height='auto' />
+				</DonateLink>
 			</NavContainer>
 		</Container>
 	);
