1c27a91c8498835cb7adccf9ddaafe2619f58bac
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-20 10:42:27 -0400

    feat: styling compoleted for About

--- components/About.jsx
+++ components/About.jsx
@@ -1,42 +1,98 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+	padding-left: 0.25rem;
+	padding-right: 0.25rem;
+
+	@media (min-width: 640px) {
+		padding-left: 1.5rem;
+		padding-right: 1.5rem;
+	}
+
+	@media (min-width: 1024px) {
+		width: 75%;
+		margin-top: 2.5rem;
+	}
+`;
+
+const FlexContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+
+	@media (min-width: 1024px) {
+		justify-content: space-between;
+	}
+`;
+
+const AboutContainer = styled.div`
+	padding: 1.75rem;
+	@media (min-width: 1024px) {
+		width: 50%;
+		padding: 0px;
+	}
+`;
+
+const Header = styled.h1`
+	font-size: 1.875rem;
+	line-height: 2.25rem;
+	text-align: center;
+	margin-bottom: 1.25rem;
+	color: rgb(37 99 235);
+
+	@media (min-width: 640px) {
+		font-size: 3.75rem;
+		line-height: 1;
+		margin-bottom: 2.5rem;
+	}
+`;
+
+const Paragraph = styled.p`
+	font-size: 0.875rem;
+	line-height: 1.625rem;
+	padding-bottom: 1.25rem;
+
+	@media (min-width: 640px) {
+		font-size: 1.25rem;
+	}
+`;
+
+const ImageContainer = styled.div`
+	width: 24rem;
+	padding: 1.25rem;
+`;
 
 function About() {
 	return (
-		<div className='lg:w-3/4 px-1 sm:px-6 lg:mt-10'>
-			<div className='flex flex-row flex-wrap justify-center lg:justify-between items-center'>
-				<div className='lg:w-6/12 lg:p-0 p-7'>
-					<h1 className='text-3xl sm:text-6xl text-center mb-5 sm:mb-10 font-headers text-blue-600'>
-						Kellen Wiltshire
-					</h1>
-					<p className='text-sm sm:text-xl leading-relaxed font-body pb-5'>
-						I am a passionate full-stack developer specializing in all aspects
-						of Web Development looking for the next step in my career. I have
-						the experience to create beautiful and functional websites combining
-						the latest frameworks and technologies. I have experience in React,
-						Javascript/Typescript, TailwindCSS, Styled Components, as well as
-						traditional HTML5 and CSS3. I am currently look for a new Web
-						Development career opportunity!
-					</p>
+		<Container>
+			<FlexContainer>
+				<AboutContainer>
+					<Header>Kellen Wiltshire</Header>
+					<Paragraph>
+						I am a passionate full-stack developer specializing in all aspects of Web Development looking for the next
+						step in my career. I have the experience to create beautiful and functional websites combining the latest
+						frameworks and technologies. I have experience in React, Javascript/Typescript, TailwindCSS, Styled
+						Components, as well as traditional HTML5 and CSS3. I am currently look for a new Web Development career
+						opportunity!
+					</Paragraph>
 
-					<p className='text-sm sm:text-xl pb-5 leading-relaxed font-body'>
-						See more of my work at{' '}
-						<a href='kellenwiltshire.com'>KellenWiltshire.com</a>
-					</p>
-				</div>
+					<Paragraph>
+						See more of my work at <a href='kellenwiltshire.com'>KellenWiltshire.com</a>
+					</Paragraph>
+				</AboutContainer>
 
-				<div className='flex flex-row w-96 justify-center items-center text-center p-5 rounded-none shadow'>
-					<div>
-						<img
-							className='rounded-full'
-							src='https://avatars.githubusercontent.com/u/31140634?v=4'
-							alt='Profile Image'
-						/>
-						<h1 className='text-black'>Kellen Wiltshire</h1>
-						<h2 className='text-black'>Kingston, Ontario, Canada</h2>
-					</div>
-				</div>
-			</div>
-		</div>
+				<ImageContainer>
+					<img
+						style={{ borderRadius: '50%' }}
+						src='https://avatars.githubusercontent.com/u/31140634?v=4'
+						alt='Profile Image'
+					/>
+				</ImageContainer>
+			</FlexContainer>
+		</Container>
 	);
 }
 
